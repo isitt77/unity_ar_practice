6ac4da8b2bc0b17dab331d6b68f42d2d86380d8f
Scott Isitt <isitts@hotmail.com>
2022-09-06 22:55:38 -0700

    Create UpdatePlacementPose().

--- Assets/Scripts/AR_Cursor_2.cs
+++ Assets/Scripts/AR_Cursor_2.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,34 @@ using UnityEngine.XR.ARFoundation;
 public class AR_Cursor_2 : MonoBehaviour
 {
     ARSessionOrigin arSessionOrigin;
+    ARRaycastManager raycastManager;
     Pose placementPose;
+    bool placementPoseIsValid = false;
 
 
     void Start()
     {
         arSessionOrigin = FindObjectOfType<ARSessionOrigin>();
+        raycastManager = FindObjectOfType<ARRaycastManager>();
     }
 
     void Update()
     {
-        
+        UpdatePlacementPose();
+    }
+
+
+    void UpdatePlacementPose()
+    {
+        Vector2 screenCenter = Camera.current.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
     }
 }
